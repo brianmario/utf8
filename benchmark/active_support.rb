30e14c76d1bf482b8d9194bbc9ec25412bcab2f6
Brian Lopez <seniorlopez@gmail.com>
2011-01-11 16:35:57 -0800

    reorganize benchmark

--- benchmark/active_support.rb
+++ benchmark/active_support.rb
@@ -10,42 +10,52 @@ raw = File.read(File.expand_path('../test.txt', __FILE__))
 utf8 = raw.as_utf8
 as_mb = ActiveSupport::Multibyte::Chars.new(raw)
 
-times = 1
+times = 1000
 
+puts "String::UTF8"
 Benchmark.bmbm do |x|
   x.report {
-    puts "String::UTF8#length"
-    utf8.length
+    puts "#length"
+    times.times {utf8.length}
   }
-
   x.report {
-    puts "ActiveSupport::Multibyte::Chars#length"
-    as_mb.length
+    puts "#[index]"
+    times.times {utf8[1024]}
+  }
+  x.report {
+    puts "#[-index]"
+    times.times {utf8[-1024]}
+  }
+  x.report {
+    puts "#[start, len]"
+    times.times {utf8[1024, 1024]}
+  }
+  x.report {
+    puts "#[-start, len]"
+    times.times {utf8[-1024, 1024]}
   }
 end
 
-puts "\n\n"
+puts "\n\nActiveSupport::Multibyte::Chars"
 Benchmark.bmbm do |x|
   x.report {
-    puts "String::UTF8#[start, len]"
-    utf8[1024, 1024]
+    puts "#length"
+    times.times {as_mb.length}
   }
-
   x.report {
-    puts "ActiveSupport::Multibyte::Chars#[start, len]"
-    as_mb[1024, 1024]
+    puts "#[index]"
+    times.times {as_mb[1024]}
   }
-end
-
-puts "\n\n"
-Benchmark.bmbm do |x|
   x.report {
-    puts "String::UTF8#[-start, len]"
-    utf8[-1024, 1024]
+    puts "#[-index]"
+    times.times {as_mb[-1024]}
   }
-
   x.report {
-    puts "ActiveSupport::Multibyte::Chars#[-start, len]"
-    as_mb[-1024, 1024]
+    puts "#[start, len]"
+    times.times {as_mb[1024, 1024]}
+  }
+  x.report {
+    puts "#[-start, len]"
+    times.times {as_mb[-1024, 1024]}
   }
 end
