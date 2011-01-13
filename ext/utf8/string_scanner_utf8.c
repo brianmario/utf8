3b76ca87bf81045a4d382ad2daacd23a61fc858b
Brian Lopez <seniorlopez@gmail.com>
2011-01-13 01:53:06 -0800

    no need to override StringScanner#reset

--- ext/utf8/string_scanner_utf8.c
+++ ext/utf8/string_scanner_utf8.c
@@ -27,40 +27,22 @@ static VALUE rb_cStringScanner_UTF8_getch(VALUE self) {
   unsigned char *str;
   size_t len;
   struct strscanner *scanner;
-  VALUE rb_raw_offset, utf8Str;
-  size_t raw_offset = 0;
+  VALUE utf8Str;
   int8_t lastCharLen=0;
   GET_SCANNER(self, scanner);
-
-  rb_raw_offset = rb_iv_get(self, "raw_offset");
-  if (NIL_P(rb_raw_offset)) {
-    rb_iv_set(self, "raw_offset", INT2FIX(0));
-  } else {
-    raw_offset = FIX2ULONG(rb_raw_offset);
-  }
 
   str = (unsigned char *)RSTRING_PTR(scanner->str);
   len = RSTRING_LEN(scanner->str);
 
-  if (len > 0 && len > raw_offset) {
+  if (len > 0 && len > scanner->curr) {
     lastCharLen = utf8CharLen(str, len);
-    utf8Str = rb_str_new((char *)str+raw_offset, lastCharLen);
-    raw_offset += lastCharLen;
-    rb_iv_set(self, "raw_offset", LONG2FIX(raw_offset));
+    utf8Str = rb_str_new((char *)str+scanner->curr, lastCharLen);
+    scanner->curr += lastCharLen;
     AS_UTF8(utf8Str);
     return utf8Str;
   } else {
     return Qnil;
   }
-}
-
-static VALUE rb_cStringScanner_UTF8_reset(int argc, VALUE * argv, VALUE self) {
-  VALUE retVal;
-
-  retVal = rb_call_super(argc, argv);
-  rb_iv_set(self, "raw_offset", Qnil);
-
-  return retVal;
 }
 
 void init_StringScanner_UTF8() {
@@ -74,5 +56,4 @@ void init_StringScanner_UTF8() {
   rb_cStringScanner_UTF8 = rb_define_class_under(rb_cStringScanner, "UTF8", rb_cStringScanner);
 
   rb_define_method(rb_cStringScanner_UTF8, "getch", rb_cStringScanner_UTF8_getch, 0);
-  rb_define_method(rb_cStringScanner_UTF8, "reset", rb_cStringScanner_UTF8_reset, -1);
 }
