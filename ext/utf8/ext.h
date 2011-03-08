e244f3877bfe86ab3a8173b373f741edf9e5b68d
Brian Lopez <seniorlopez@gmail.com>
2011-03-08 15:25:00 -0800

    define RSTRING_NOT_MODIFIED for Rubinius
    By doing this we're making a promize to Rubinius that we'll never modify the pointers we get back from RSTRING_PTR
    This gives us a little speed up as Rubinius won't have to track a cached version of the pointer

--- ext/utf8/ext.h
+++ ext/utf8/ext.h
@@ -1,6 +1,11 @@
 #ifndef UTF8_EXT_H
 #define UTF8_EXT_H
 
+// tell rbx not to use it's caching compat layer
+// by doing this we're making a promize to RBX that
+// we'll never modify the pointers we get back from RSTRING_PTR
+#define RSTRING_NOT_MODIFIED
+
 #include <ruby.h>
 
 #ifdef HAVE_RUBY_ENCODING_H
