368754102ebf0fd3e6947b35b9d801a91837a33a
Brian Lopez <seniorlopez@gmail.com>
2011-06-01 23:51:44 -0700

    require version too

--- lib/utf8.rb
+++ lib/utf8.rb
@@ -1,5 +1,6 @@
 require 'utf8/utf8'
 require 'utf8/string'
+require 'utf8/version' unless defined? String::UTF8::VERSION
 
 # explicitly require this in your app if you want to use it
 # require 'utf8/string_scanner'
